41ecfad9c79a6b736c67711b896b97e6ff156650
Georgii Musikhin <sadjesty@gmail.com>
2020-10-11 20:57:05 +0300

    Second commit

---
+++ src/main/java/com/longdivision/DivisionApplication.java
@@ -0,0 +1,23 @@
+package com.longdivision;
+
+import java.util.Scanner;
+
+public class DivisionApplication {
+
+	private static DivisionModel model;
+	private static DivisionPrinter printer;
+
+	public static void main(String[] args) {
+		try(Scanner sc = new Scanner(System.in)) {
+			System.out.print("Intput divident: ");
+			int divident = sc.nextInt();
+			System.out.print("Intput diviser: ");
+			int diviser = sc.nextInt();
+			model = new DivisionModel(divident, diviser);
+			model.fillListOfSteps();
+			printer = new DivisionPrinter(model);
+			printer.printBase();
+		}
+	}
+
+}
--- src/main/java/com/longdivision/DivisionPrinter.java
+++ src/main/java/com/longdivision/DivisionPrinter.java
@@ -3,18 +3,19 @@ package com.longdivision;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FormatterIntegerDivision {
+public class DivisionPrinter {
 	private String divident;
 	private String diviser;
 	private String quotient;
 	private List<DivisionStep> divisionSteps = new ArrayList<DivisionStep>();
-	
-	public FormatterIntegerDivision (DivisionModel model) {
+
+	public DivisionPrinter(DivisionModel model) {
 		divident = Integer.toString(model.getDivident());
 		diviser = Integer.toString(model.getDiviser());
 		quotient = Integer.toString(model.getQuotient());
 		divisionSteps = model.getDivisionSteps();
 	}
+
 	public void printBase() {
 		DivisionStep firstStep = divisionSteps.get(0);
 		String firstIndent = Integer.toString(firstStep.getIndent());
@@ -22,12 +23,12 @@ public class FormatterIntegerDivision {
 		String firstStringFormatter = "_%s|%s\n";
 		String space = countSpaces((divident.length() + 1), firstIndent.length());
 		String nextStringsormatter = " %" + firstIndent.length() + "s%" + space + "s%s%n";
-				
+
 		System.out.printf(firstStringFormatter, divident, diviser);
 		System.out.printf(nextStringsormatter, firstMult, "|", countLines(quotient));
-		System.out.printf(nextStringsormatter,countLines(firstMult), "|", quotient);
-		
-		if(divisionSteps.size() > 1) {
+		System.out.printf(nextStringsormatter, countLines(firstMult), "|", quotient);
+
+		if (divisionSteps.size() > 1) {
 			printDivision();
 		} else {
 			String firstRemiinder = Integer.toString(firstStep.getRemainder());
@@ -38,7 +39,7 @@ public class FormatterIntegerDivision {
 	}
 
 	public void printDivision() {
-		for(int i = 1; i < divisionSteps.size(); i++) {
+		for (int i = 1; i < divisionSteps.size(); i++) {
 			DivisionStep previousStep = divisionSteps.get(i - 1);
 			DivisionStep step = divisionSteps.get(i);
 			String prevIndent = Integer.toString(previousStep.getIndent());
@@ -49,34 +50,30 @@ public class FormatterIntegerDivision {
 			System.out.printf(stringFormatter, nextIndent);
 			System.out.printf(stringFormatter, nextMult);
 			System.out.printf(stringFormatter, countLines(nextIndent));
-			
-			if((i + 1) == divisionSteps.size()) {
+
+			if ((i + 1) == divisionSteps.size()) {
 				String reminder = Integer.toString(step.getRemainder());
 
 				System.out.printf(stringFormatter, reminder);
 			}
 		}
 	}
+
 	public static String countLines(String s) {
 		StringBuilder result = new StringBuilder("");
-		
-		for(int i = 0; i < s.length(); i++) {
+
+		for (int i = 0; i < s.length(); i++) {
 			result.append("-");
 		}
 		return result.toString();
 	}
-	public static String countSpaces (int firstString, int secondString) {
-		String result = "";
-		if(firstString !=  secondString) {
-			result = Integer.toString(firstString -  secondString);
-		}
-		return result;
-	}
 
-	public static void main(String[] args) {
-		DivisionModel model = new DivisionModel(59678, 600);
-		model.fillListOfSteps();
-		FormatterIntegerDivision form = new FormatterIntegerDivision(model);
-		form.printBase();
+	public static String countSpaces(int firstString, int secondString) {
+		String result = "";
+		
+		if (firstString != secondString) {
+			result = Integer.toString(firstString - secondString);
+		} 
+		return result;
 	}
 }
